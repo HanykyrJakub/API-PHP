09108f9e406867ded63dedc27db2d96c756f259a
HanykyrJakub <90202111+HanykyrJakub@users.noreply.github.com>
2022-12-29 13:41:29 +0100

    Add files via upload

--- Api/F1/index.php
+++ Api/F1/index.php
@@ -1,10 +1,30 @@
 <?php
-if($number === null) {
-    reply(["error" => "no Number"]);
+$result = 0;
+$operation = filter_input(INPUT_GET, 'operation');
+$num1 = filter_input(INPUT_GET, 'num1');
+$num2 = filter_input(INPUT_GET, 'num2');
+
+//operations
+if($operation == "add"){
+    $num1 + $num2;
+    json_encode(["result" => $result]);
 }
-if($number === false) {
-    reply(["error" => "not a Number"]);
+if($operation == "sub"){
+    $num1 - $num2;
+    json_encode(["result" => $result]);
+}
+if($operation == "multi"){
+    $num1 * $num2;
+    json_encode(["result" => $result]);
+}
+if($operation == "divid"){
+    $num1 / $num2;
+    json_encode(["result" => $result]);
 }
 
+//error
+if($num1 != is_numeric($num1) || $num2 != is_numeric($num2)){
+    echo json_encode(["error" => "not a number"]);
+}
 ?>
 
--- Api/F3/server.php
+++ Api/F3/server.php
@@ -1,8 +1,15 @@
 <?php
-$url = "http://localhost/my-app/Api/";
+header('content-type: application/json; charset = utf-8');
 
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array('accept:application/json', ))
 
+$param = explode("/",str_replace("http://localhost/my-app/Api/F3", "",
+$_SERVER['REQUEST_URI']));
+
+$oper = array_shift($param);
+echo json_encode(
+    [
+        "oper" => $oper,
+        "post" = json_decode(file_get_contents('Api://input'), true)
+    ]
+    );
 ?>
